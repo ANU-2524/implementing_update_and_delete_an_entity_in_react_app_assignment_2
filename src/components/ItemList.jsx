a1e9a31b2d526fd68c781ef89ad900a6876400fa
ANU-2524 <anusoni25.2006@gmail.com>
2025-03-31 11:01:55 +0530

    Delete Button Added

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,11 +1,12 @@
-import Item from "./Item";
+
 
 const ItemList = ({ items  , onDelete}) => {
     return (
         <>
-            {items.map((item) => (
-                <Item key={item.id} item={item} onDelete={onDelete}/>
-            ))}
+           {items?.map((item) => (
+    <Item key={item.id} item={item} onDelete={onDelete} />
+))}
+
         </>
     );
 };
